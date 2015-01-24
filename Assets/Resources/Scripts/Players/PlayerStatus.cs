a7e3319e696f7014ec558581b6ed4aabb63d9e1b
Jose <polonoise@gmail.com>
2015-01-24 12:01:34 -0430

    status

--- Assets/Resources/Scripts/Players/PlayerStatus.cs
+++ Assets/Resources/Scripts/Players/PlayerStatus.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class PlayerStatus : MonoBehaviour {
+public class PlayerStatus : MonoBehaviour
+{
+    private float _health;
 
-	// Use this for initialization
-	void Start () {
-	
+    public float MaxHealth = 1.0f;
+    public float MinHealth = 0.0f;
+    public float Speed = 1.0f;
+
+    public List<StatusBehaviour> PlayerStatuses;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    this._health = this.MaxHealth;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void ReduceHealth(float value)
+    {
+        this._health = Mathf.Clamp(this._health - value, this.MinHealth, this.MaxHealth);
+    }
+
+    void RecoverHealth(float value)
+    {
+        this._health = Mathf.Clamp(this._health + value, this.MinHealth, this.MaxHealth);
+    }
 }
---
+++ Assets/Resources/Scripts/Status/StatusBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusBehaviour : MonoBehaviour {
+
+    public string Identifier;
+    public float Duration;
+
+    public UnityEditor.MonoScript BehaviourScript;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
