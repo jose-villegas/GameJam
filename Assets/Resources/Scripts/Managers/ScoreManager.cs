3cb9430cc1885d2dd1982679cdcfa7425c1068bc
Jose <polonoise@gmail.com>
2015-01-24 10:44:46 -0430

    score manager implementation

--- Assets/Resources/Scripts/Managers/ScoreManager.cs
+++ Assets/Resources/Scripts/Managers/ScoreManager.cs
@@ -4,22 +4,42 @@ using System.Collections;
 public class ScoreManager : MonoBehaviour {
 	// Singleton
 	static private ScoreManager _instance;
+
+    public float CurrentScore = 0.0f;
+    public float ScoreMultiplier = 1.0f;
+    public float HighScore;
+
 	static public ScoreManager Instance
 	{
 		get{return _instance;}
 	}
+
+    void AddToScore(float value)
+    {
+        CurrentScore += value * this.ScoreMultiplier;
+    }
+
+    void SaveHighScore()
+    {
+        if (this.CurrentScore > this.HighScore)
+        {
+            PlayerPrefs.SetFloat("High Score", this.HighScore);
+        }
+    }
+
 	ScoreManager()
 	{
 		_instance = this;
 	}
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    this.HighScore = PlayerPrefs.GetFloat("High Score");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 }
