2d1c25943ebc065522acbc0e2a3c823ce15d9c4a
neroziros <nero_lucho92@hotmail.com>
2015-01-24 19:03:20 -0430

    A LOT OF STUFF

---
+++ Assets/Resources/Scripts/Building/ObjectiveArrow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ObjectiveArrow : MonoBehaviour {
+	private Sequence HelpSequence;
+
+	// Use this for initialization
+	void Start () {
+		// Create help animaton sequence
+		HelpSequence = DOTween.Sequence ();
+		float InitialLocalY = transform.localPosition.y;
+		HelpSequence.Append (transform.DOLocalMoveY (InitialLocalY + 0.5f, 0.5f));
+		HelpSequence.Append (transform.DOLocalMoveY (InitialLocalY, 0.5f));
+		HelpSequence.SetLoops (-1);
+		HelpSequence.Play ();
+
+		transform.DORotate (new Vector3(0,360.0f,0), 1.0f, RotateMode.LocalAxisAdd).SetLoops (-1);
+	}
+}
---
+++ Assets/Resources/Scripts/Main Menu/ScoreMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreMenuManager : MonoBehaviour {
+	// Scene transitions
+	public string MainMenu = "Main Menu";
+	// Use this for initialization
+	void Start () {
+		if(ScoreManager.Instance == null)
+			return;
+	}
+	
+	/// <summary>
+	/// Goes to game.
+	/// </summary>
+	/// <param name="choice">Choice.</param>
+	public void GoToMainMenu()
+	{
+		Application.LoadLevel (MainMenu);	
+	}
+}
