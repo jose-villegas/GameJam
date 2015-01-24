d7ceab6f0a787993d023606df9ff5724957fdcaf
neroziros <nero_lucho92@hotmail.com>
2015-01-23 22:04:09 -0430

    Main Manager Scripts

---
+++ Assets/Resources/Scripts/Managers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Game manager: Controls the main game flow
+/// </summary>
+public class GameManager : MonoBehaviour {
+	// Singleton
+	static private GameManager _instance;
+	static public GameManager Instance
+	{
+		get{return _instance;}
+	}
+	GameManager()
+	{
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Resources/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+	// Singleton
+	static private ScoreManager _instance;
+	static public ScoreManager Instance
+	{
+		get{return _instance;}
+	}
+	ScoreManager()
+	{
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
