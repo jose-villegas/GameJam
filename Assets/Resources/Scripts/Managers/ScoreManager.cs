bc41fa3afb4312b9dad01c901e8beee249ee584e
Jose <polonoise@gmail.com>
2015-01-24 10:56:44 -0430

    saving score name

--- Assets/Resources/Scripts/Managers/ScoreManager.cs
+++ Assets/Resources/Scripts/Managers/ScoreManager.cs
@@ -9,22 +9,29 @@ public class ScoreManager : MonoBehaviour {
     public float ScoreMultiplier = 1.0f;
     public float HighScore;
 
+    private string _highScorer;
+
 	static public ScoreManager Instance
 	{
 		get{return _instance;}
 	}
+
+    string GetHighestScoreName()
+    {
+        return this._highScorer;
+    }
 
     void AddToScore(float value)
     {
         CurrentScore += value * this.ScoreMultiplier;
     }
 
-    void SaveHighScore()
+    void SaveHighScore(string scorer)
     {
-        if (this.CurrentScore > this.HighScore)
-        {
-            PlayerPrefs.SetFloat("High Score", this.HighScore);
-        }
+        if (!(this.CurrentScore > this.HighScore)) return;
+
+        PlayerPrefs.SetFloat("High Score", this.HighScore);
+        PlayerPrefs.SetString("High Scorer", scorer);
     }
 
 	ScoreManager()
@@ -35,7 +42,8 @@ public class ScoreManager : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-	    this.HighScore = PlayerPrefs.GetFloat("High Score");
+	    this.HighScore = PlayerPrefs.GetFloat("High Score", 0.0f);
+	    this._highScorer = PlayerPrefs.GetString("High Scorer", "Player");
 	}
 	
 	// Update is called once per frame
