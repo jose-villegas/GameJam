1d9bc008e92e072a95277e2d00a23ad6a67c1e7a
neroziros <nero_lucho92@hotmail.com>
2015-01-24 16:14:42 -0430

    Invisible Wall

---
+++ Assets/Resources/Scripts/Building/InvisibleWall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class InvisibleWall : MonoBehaviour {
+
+	public LayerMask CitizensLayer;
+	public LayerMask FlyingCitizensLayer;
+
+	void OnTriggerEnter(Collider collider)
+	{
+		Debug.Log(collider);
+		if (FlyingCitizensLayer == (FlyingCitizensLayer | (1 << collider.gameObject.layer)) ||
+		    CitizensLayer == (CitizensLayer | (1 << collider.gameObject.layer)))
+		{
+				collider.gameObject.transform.DOPlayBackwards();
+		}
+	}
+}
