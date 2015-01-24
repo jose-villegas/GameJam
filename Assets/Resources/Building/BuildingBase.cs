69f61a58668d55e5cd7ba6d68d93dc16f97662de
Jose <polonoise@gmail.com>
2015-01-24 13:57:16 -0430

    buildins and secondary player holder

---
+++ Assets/Resources/Building/BuildingBase.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BuildingBase : MonoBehaviour
+{
+    // every building has a collection of current living residents
+    private List<SecondaryPlayer> _residents;
+    // buildings constrains
+    public int MaxResidents = 5;
+
+    // Use this for initialization
+	void Start () {
+	    this._residents = new List<SecondaryPlayer>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void AddResident(SecondaryPlayer newResident)
+    {
+        if (this._residents.Count >= this.MaxResidents) return;
+
+        this._residents.Add(newResident);
+    }
+
+    void ClearBuilding()
+    {
+        this._residents.Clear();
+    }
+}
