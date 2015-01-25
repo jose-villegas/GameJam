11b6a1839ad562e6788d818ccca0c0e1401f656b
Jose <polonoise@gmail.com>
2015-01-25 05:10:44 -0430

    add

---
+++ Assets/Resources/Scripts/Enemy/PlayerDetection.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDetection : MonoBehaviour
+{
+    // sphere cast layer
+    public LayerMask PlayerMask;
+
+    private RaycastHit _sphereRay;
+    // Use this for initialization
+    private void Start()
+    {
+        // sphere ray cast layer
+        this.PlayerMask = LayerMask.GetMask("Player");
+    }
+
+    // Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(PlayerMask == (PlayerMask | (1 << collider.gameObject.layer)))
+        {
+            Debug.Log(collider.gameObject);
+        }
+    }
+}
