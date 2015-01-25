6b7dcd1b07bc1d387340e3a07ed12cc1b973f74b
neroziros <nero_lucho92@hotmail.com>
2015-01-25 12:30:09 -0430

    PHANTOM

--- Assets/Resources/Scripts/Enemy/VerticalSwayAnimationm.cs
+++ Assets/Resources/Scripts/Enemy/VerticalSwayAnimationm.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class VerticalSwayAnimationm : MonoBehaviour {
+	private Sequence HelpSequence;
 
 	// Use this for initialization
 	void Start () {
-	
+		// Create help animaton sequence
+		HelpSequence = DOTween.Sequence ();
+		float InitialLocalY = transform.localPosition.y;
+		HelpSequence.Append (transform.DOLocalMoveY (InitialLocalY+1.0f, 0.5f));
+		HelpSequence.Append (transform.DOLocalMoveY (InitialLocalY, 0.5f));
+		HelpSequence.SetLoops (-1);
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
