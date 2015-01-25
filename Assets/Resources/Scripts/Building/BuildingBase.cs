883ca9ec5421b70266c9d33770eba9e0268d329d
Jose <polonoise@gmail.com>
2015-01-25 01:12:59 -0430

    building prefab

--- Assets/Resources/Scripts/Building/BuildingBase.cs
+++ Assets/Resources/Scripts/Building/BuildingBase.cs
@@ -69,6 +69,7 @@ public class BuildingBase : MonoBehaviour
 
     void OnTriggerEnter(Collider collider)
     {
+        Debug.Log(collider);
         if (FlyingCitizensLayer == (FlyingCitizensLayer | (1 << collider.gameObject.layer)) ||
             CitizensLayer == (CitizensLayer | (1 << collider.gameObject.layer)))
         {
