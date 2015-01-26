c785bfec9a9d401f83011230aa27b5609ebc621b
Jose <polonoise@gmail.com>
2015-01-25 22:16:23 -0430

    chiguire ai testing

--- Assets/Resources/Scripts/Enemy/ChargingChiguire.cs
+++ Assets/Resources/Scripts/Enemy/ChargingChiguire.cs
@@ -6,6 +6,8 @@ public class ChargingChiguire : MonoBehaviour {
 
     public float Speed = 2.0f;
 
+    public float ChargingSpeed = 5.0f;
+
     public float MaxChangeDirectionFrequency = 2.0f;
 
     public float MaxStayStillChangeFrequency = 3.0f;
@@ -58,14 +60,38 @@ public class ChargingChiguire : MonoBehaviour {
 
     private Vector3 _chargingDirection;
     private Vector3 _chargingFinalPosition;
-    private bool _chargingSet = false;
+    private float _chargingTimeSpend = 0.0f;
+    private float _chargingTimeStop = 2.0f;
+    private bool _chargingSet = true;
+    private float _lastChargeTime = 0.0f;
 
-    private void SetChargingDirection()
+    public float TimeBetweenCharges = 2.0f;
+
+    private bool SetChargingDirection()
     {
-        if (_chargingSet) return;
+        if (Time.fixedTime - _lastChargeTime < TimeBetweenCharges)
+        {
+            _chargingSet = false;
+            return false;
+        }
+
+        // charging move once we are pass the target position (or we are beyond the charging time)
+        float distanceToFinalPos = (transform.position - _chargingFinalPosition).sqrMagnitude;
+
+        if (distanceToFinalPos <= 0.1 || _chargingTimeSpend > _chargingTimeStop  && _chargingSet)
+        {
+            _chargingSet = false;
+            return false;
+        }
+
 
         _chargingSet = true;
         _chargingDirection = (Player.transform.position - transform.position).normalized;
+        _chargingFinalPosition = Player.transform.position;
+        _chargingTimeSpend = 0.0f;
+        _lastChargeTime = Time.fixedTime;
+
+        return true;
     }
 
     // Update is called once per frame
@@ -73,15 +99,13 @@ public class ChargingChiguire : MonoBehaviour {
     {
         DistanceToPlayer = (Player.transform.position - transform.position).sqrMagnitude;
 
-        if (DistanceToPlayer <= StartFollowingDistance * StartFollowingDistance || _chargingSet)
+        if (DistanceToPlayer <= StartFollowingDistance * StartFollowingDistance && SetChargingDirection())
         {
-            // set to charging mode
-            this.SetChargingDirection();
             // move to player position on detection
-            _Character.Move(this._chargingDirection * Time.deltaTime * Speed);
+            _Character.Move(this._chargingDirection * Time.deltaTime * ChargingSpeed);
             transform.position = new Vector3(transform.position.x, _initialY, transform.position.z);
-            // charging move once we are pass the target position (or we are beyond the charging time)
-            
+            // sum to time spend charging mode
+            _chargingTimeSpend += Time.deltaTime;
         } 
         else if (!this.StayStill && _Character != null && GameManager.Instance.GameState == GameManager.GameStatus.Playing)
         {
